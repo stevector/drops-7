5ccd00dc0937ba1706c3ed41b309b3890dc7173e
Steve Persch <steve.persch@pantheon.io>
2017-02-08 10:55:16 -0600

    Adding create-env

---
+++ test/create-env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit immediately on errors, and echo commands as they are executed.
+# Once CircleCI 2.0 is out, these are commands that would move to a shared
+# Dockerfile
+set -e
+
+# drops-7 does not provide a settings.php file
+cp sites/default/default.settings.php sites/default/settings.php
+
+terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
+terminus build-env:delete:ci -n "$TERMINUS_SITE" --keep=4 --yes
+terminus env:wake -n "$TERMINUS_SITE.dev"
+terminus build-env:create -n "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --yes
